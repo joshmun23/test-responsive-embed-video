24b3cfaf22820b95d588f39f580202ee2f61b181
Joshua Mun <joshmun23@gmail.com>
2015-09-16 12:49:26 -0400

    create objects to store and update variables based off of screen sizes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,14 +17,58 @@
 
 $(function(){ $(document).foundation(); });
 
-var height = $(window).height(),
-    width  = $(window).width(),
-    container = $('.videoWrapper'),
+var saVidWidth  = $(window).width(),
+    saVidHeight = $(window).height(),
+    container = $('.videoWrapper');
 
-container.html('<iframe width="' + width + '" height="'+ height
-  + '" frameborder="0" allowfullscreen src="http://www.necn.com
-  /templates/nbc_partner_player?cmsID=326853441&amp;videoID=DKt
-  5Dc73cWfn&amp;origin=necn.com&amp;sec=news&amp;subsec=business
-  &amp;width=' + width + '&amp;height=' + height + '&amp;turl=
-  http%3A%2F%2Flocalhost%3A3000%2F&amp;ourl=http%3A%2F%2Flocalhost
-  %3A3000&amp;lp=1" style="border: none;"></iframe>')
+var saVidDimensions = {
+  width: saVidWidth,
+  height: saVidHeight
+};
+
+console.log('width: ' + saVidWidth);
+console.log('height: ' + saVidHeight);
+
+var saVidBreakpoints = {
+  small: {
+    width: 300,
+    height: saVidHeight * 0.50
+  },
+  medium: {
+    width: 768,
+    height: saVidHeight * 0.42
+  },
+  large: {
+    width: 1024,
+    height: saVidHeight * 0.33
+  }
+};
+
+console.log(saVidBreakpoints.medium.width);
+console.log(saVidBreakpoints.medium.height);
+
+// set width & height based on screen size
+if (saVidWidth >= 1024){
+  console.log('large screen')
+  saVidDimensions.width = saVidWidth * 0.90;
+  saVidDimensions.height = saVidHeight * 0.50;
+} else if (saVidWidth < 1024 && saVidWidth >= 768) {
+  console.log('medium screen')
+  saVidDimensions.width = saVidWidth * 0.73;
+  saVidDimensions.height = saVidHeight * 0.42;
+} else if (saVidWidth < 768 && saVidHeight > 300) {
+  console.log('small screen')
+  saVidDimensions.width = saVidWidth * 0.66;
+  saVidDimensions.height = saVidHeight * 0.33;
+};
+
+console.log(saVidDimensions.width)
+console.log(saVidDimensions.height)
+
+// container.html('<iframe width="' + width + '" height="'+ height
+//   + '" frameborder="0" allowfullscreen src="http://www.necn.com
+//   /templates/nbc_partner_player?cmsID=326853441&amp;videoID=DKt
+//   5Dc73cWfn&amp;origin=necn.com&amp;sec=news&amp;subsec=business
+//   &amp;width=' + width + '&amp;height=' + height + '&amp;turl=
+//   http%3A%2F%2Flocalhost%3A3000%2F&amp;ourl=http%3A%2F%2Flocalhost
+//   %3A3000&amp;lp=1" style="border: none;"></iframe>')
